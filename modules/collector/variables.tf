384b8541a79b6ab7ae1462442c365dc834bcbb5b
Ryan Tiffany <ryantiffany@fastmail.com>
2022-07-13 13:06:48 -0500

    splitting bp and modules

--- modules/collector/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "frontend_subnet_id" {}
-variable "backend_subnet_id" {}
-variable "backend_collector_bucket" {}
-variable "frontend_collector_bucket" {}
-variable "name" {}
-variable "resource_group_id" {}
